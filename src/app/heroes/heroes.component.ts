b61a2f55a21f4a4d27e879d4c32df3c080b5eeb7
Takeaki <takeaki.yamamto@gmail.com>
2023-01-19 11:15:02 +0900

    リストを表示

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -12,6 +13,13 @@ export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1, 
     name: 'Windstorm'
+  }
+
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
   constructor() { }
